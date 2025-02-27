ee3763458b412ee9d9299375e1c7355cfc0a02e4
Amarothia <serenitygfox@gmail.com>
2025-02-27 02:41:15 -0800

    added function split_nodes_delimiter to textnode.py and more tests covering it to test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -34,6 +34,55 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.LINK, "url")
         node2 = TextNode("This is a text node", TextType.LINK)
         self.assertNotEqual(str(node), str(node2))
+
+    # Set of tests for deliminter splitting of old TextNodes
+
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("This is my **boom** stick!", TextType.TEXT)
+        node1 = TextNode("This is my ", TextType.TEXT)
+        node2 = TextNode("boom", TextType.BOLD)
+        node3 = TextNode(" stick!", TextType.TEXT)
+        html_node_list = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(html_node_list, [node1, node2, node3])
+
+    def test_split_nodes_delimiter_italics_first_word(self):
+        node = TextNode("_The Necronomicon_, a book for all ages!", TextType.TEXT)
+        node1 = TextNode("The Necronomicon", TextType.ITALIC)
+        node2 = TextNode(", a book for all ages!", TextType.TEXT)
+        html_node_list = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(html_node_list, [node1, node2])
+
+    def test_split_nodes_delimiter_code_last_word(self):
+        node = TextNode("I'm the man with the plan, here's my code `typety typety type type`", TextType.TEXT)
+        node1 = TextNode("I'm the man with the plan, here's my code ", TextType.TEXT)
+        node2 = TextNode("typety typety type type", TextType.CODE)
+        html_node_list = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(html_node_list, [node1, node2])
+
+    def test_split_nodes_delimiter_multiple_words(self):
+        node = TextNode("This is my **boom** stick, beware my **wrath**!", TextType.TEXT)
+        node1 = TextNode("This is my ", TextType.TEXT)
+        node2 = TextNode("boom", TextType.BOLD)
+        node3 = TextNode(" stick, beware my ", TextType.TEXT)
+        node4 = TextNode("wrath", TextType.BOLD)
+        node5 = TextNode("!", TextType.TEXT)
+        html_node_list = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(html_node_list, [node1, node2, node3, node4, node5])
+
+    def test_split_nodes_delimiter_not_TEXT_type(self):
+        node = TextNode("This is my **boom** stick!", TextType.BOLD)
+        html_node_list = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(html_node_list, [node])
+
+    def test_split_nodes_delimiter_unmatched_delimiters(self):
+        node = TextNode("This is my **boom stick!", TextType.TEXT)
+        with self.assertRaises(ValueError) as context:
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertTrue("unmatched" in str(context.exception).lower())
+
+    def test_split_nodes_delimiter_empty_node_list(self):
+        html_node_list = split_nodes_delimiter([], "**", TextType.BOLD)
+        self.assertEqual(html_node_list, [])
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -21,3 +21,29 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def  split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType):
+    # This function only handles bold, italics and code. Image and Link will be handled later or in a different function.
+    new_nodes = []
+
+    for node in old_nodes: # node is a TextNode
+        sub_nodes = []
+        counter = 2
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            if delimiter in node.text:
+                sub_nodes = node.text.split(delimiter) # breaking node's text value into a sub list
+                if len(sub_nodes) % 2 == 0:
+                    raise ValueError(f"Unmatched delimiters found in text: {node.text}")
+                for sub_node_string in sub_nodes: # using that string
+                    if counter % 2 == 0: # this accomodates any number of special words in the text
+                        if sub_node_string != "":
+                            new_nodes.append(TextNode(sub_node_string, TextType.TEXT))
+                    else:
+                        new_nodes.append(TextNode(sub_node_string, text_type))
+                    counter += 1
+            else:
+                new_nodes.append(node)
+
+    return new_nodes
